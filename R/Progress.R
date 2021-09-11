50ce36a92ae9a5f4dca93c2f93fb39d400deff48
Xiang Li <xiangli2pro@gmail.com>
2021-09-11 15:47:45 -0400

    start building the package hbcmq

---
+++ R/Progress.R
@@ -0,0 +1,13 @@
+
+
+## 1.1 Check github setup
+# git config --global --list
+# git config --global user.name "YOUR FULL NAME"
+# git config --global user.email "YOUR EMAIL ADDRESS"
+
+## 1.2 From R, you can check if you already have an SSH key-pair by running
+file.exists("~/.ssh/id_rsa.pub")
+
+## 1.3 create a new repo locally
+# In a shell, run git init. Restart RStudio and reopen your package
+
