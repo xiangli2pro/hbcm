fcf94025c1664631767dc434b253f44166e6223e
xiangli2pro <xiangli2pro@gmail.com>
2023-08-13 21:13:06 -0400

    add experiment function with constant hlambda and hsigma

--- R/experiment_functions.R
+++ R/experiment_functions.R
@@ -169,6 +169,164 @@ heterogbcm_hparam <- function(x, centers, tol, iter, labels, verbose = FALSE,
   )
 }
 
+#' @rdname experiment_functions
+#' @export
+#' @description
+#' `heterogbcm_constant_hlambda_hsigma( )` experiment function to keep hlambda and hsigma constant across simulation
+heterogbcm_constant_hlambda_hsigma <- function(x, centers, tol, iter, iter_init = 3, labels, verbose = FALSE) {
+  
+  # make the data to be matrix
+  x <- as.matrix(x)
+  
+  n <- nrow(x)
+  p <- ncol(x)
+  
+  # initial values of hlambda and hsigma
+  hlambda_const <- colSums(t(x) %*% x) / (n * p)
+  hsigma_const <- rep(1, p)
+  hlambda <- hlambda_const
+  hsigma <- hsigma_const
+  # official version
+  # init_hparameters <- init_hparam(x, centers, labels, tol, iter_init, verbose)
+  # hlambda <- init_hparameters$hlambda
+  # hsigma <- init_hparameters$hsigma
+  
+  # if centers == 1, omega, ppi and qc0 are fixed
+  if (centers == 1) {
+    
+    omega <- 1
+    ppi <- 1
+    qc0 <- rep(1, p)
+    
+  } else {
+    
+    # initial estimate of group-correlation matrix omega
+    omega <- init_omega(x, centers, labels, hlambda, hsigma)
+    # initial estimate of the probablity of the multi-nulli distribution
+    ppi <- table(labels) / p
+    # initial distribution of c based on ppi
+    qc0 <- sapply(labels, function(grp) grp == c(1:centers)) * 1
+  }
+  
+  # initial distribution of alpha based on omega, qc0, hlambda, hsigma
+  qalpha <- obj_qalpha(x, centers, omega, qc0, hlambda, hsigma)
+  
+  # initial distribution of c based on omega, qalpha
+  qc <- obj_qc(x, centers, ppi, omega, qalpha, hlambda, hsigma)
+  
+  # initial -logL
+  obj_logL_val <- vector()
+  obj_logL_val[1] <- obj_logL(
+    x, centers, ppi, omega,
+    qc, qalpha,
+    hlambda, hsigma
+  )
+  
+  min_val <- obj_logL_val[1]
+  iiter <- 1
+  
+  while (iiter <= iter) {
+    
+    # update ppi
+    ppi_new <- obj_ppi(centers, qc)
+    
+    min_val <- verbose_print(
+      verbose, "ppi", min_val,
+      x, centers, ppi_new, omega,
+      qc, qalpha,
+      hlambda, hsigma
+    )
+    
+    
+    # update omega
+    omega_new <- obj_omega(centers, qalpha)
+    
+    min_val <- verbose_print(
+      verbose, "omega", min_val,
+      x, centers, ppi_new, omega_new,
+      qc, qalpha,
+      hlambda, hsigma
+    )
+    
+    
+    # update hlambda
+    # official version
+    # hlambda_new <- obj_hlambda(x, centers, qc, qalpha)
+    hlambda_new <- hlambda_const
+      
+    min_val <- verbose_print(
+      verbose, "hlambda", min_val,
+      x, centers, ppi_new, omega_new,
+      qc, qalpha,
+      hlambda_new, hsigma
+    )
+    
+    
+    # update hsigma
+    # official version
+    # hsigma_new <- obj_hsigma(x, centers, qc, qalpha, hlambda_new)
+    hsigma_new <- hsigma_const
+    
+    min_val <- verbose_print(
+      verbose, "hsigma", min_val,
+      x, centers, ppi_new, omega_new,
+      qc, qalpha,
+      hlambda_new, hsigma_new
+    )
+    
+    # update qalpha
+    qalpha_new <- obj_qalpha(x, centers, omega_new, qc, hlambda_new, hsigma_new)
+    
+    min_val <- verbose_print(
+      verbose, "qalpha", min_val,
+      x, centers, ppi_new, omega_new,
+      qc, qalpha_new,
+      hlambda_new, hsigma_new
+    )
+    
+    
+    # update qc
+    qc_new <- obj_qc(
+      x, centers, ppi_new, omega_new, qalpha_new,
+      hlambda_new, hsigma_new
+    )
+    
+    min_val <- verbose_print(
+      verbose, "qc", min_val,
+      x, centers, ppi_new, omega_new,
+      qc_new, qalpha_new,
+      hlambda_new, hsigma_new
+    )
+    
+    obj_logL_val[iiter + 1] <- obj_logL(
+      x, centers, ppi_new, omega_new,
+      qc_new, qalpha_new,
+      hlambda_new, hsigma_new
+    )
+    
+    if (abs(abs(obj_logL_val[iiter + 1] - obj_logL_val[iiter]) / obj_logL_val[iiter]) < tol) break
+    
+    iiter <- iiter + 1
+    
+    omega <- omega_new
+    hlambda <- hlambda_new
+    hsigma <- hsigma_new
+    qc <- qc_new
+    qalpha <- qalpha_new
+  }
+  
+  cluster <- apply(qc, 2, which.max)
+  
+  list(
+    omega = omega,
+    hlambda = hlambda, hsigma = hsigma,
+    obj_logL_val = -obj_logL_val, # minimize -> maximize
+    qalpha = qalpha,
+    qc = qc,
+    cluster = cluster
+  )
+}
+
 
 #' @rdname experiment_functions
 #' @export
